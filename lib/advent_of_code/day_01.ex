272fb16afe3bd85c3179d401c35a2ce83b4b9f84
dompdv <dompdv@yahoo.fr>
2023-12-02 00:30:44 +0100

    Day1 P1 & P2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,56 +1,64 @@
 defmodule AdventOfCode.Day01 do
   import Enum
 
-  @numbers ([{"eightwo", [8, 2]}]
-            |> map(fn {s, l} -> {to_charlist(s), l} end)) ++
-             ([
-                "one",
-                "two",
-                "three",
-                "four",
-                "five",
-                "six",
-                "seven",
-                "eight",
-                "nine"
-              ]
-              |> with_index()
-              |> map(fn {s, i} -> {to_charlist(s), i + 1} end))
+  # text representation of numbers, with their corresponding value
+  @numbers [
+             {"one", 1},
+             {"two", 2},
+             {"three", 3},
+             {"four", 4},
+             {"five", 5},
+             {"six", 6},
+             {"seven", 7},
+             {"eight", 8},
+             {"nine", 9}
+           ]
+           |> map(fn {s, l} -> {to_charlist(s), l} end)
 
+  # Part 1 : process one line
   def process_p1(x) do
+    # remove all non-digit characters
     x
     |> String.replace(~r/[^0-9]/, "")
     |> to_charlist()
-    |> then(fn x -> (hd(x) - ?0) * 10 + (List.last(x) - ?0) end)
+    # compute the value from first and last digits of the list
+    |> then(fn l -> (hd(l) - ?0) * 10 + (List.last(l) - ?0) end)
   end
 
   def part1(args) do
     args
     |> String.split("\n", trim: true)
+    # process each line
     |> map(&process_p1/1)
+    # sum the values
     |> sum()
   end
 
+  # Part 2
+
+  # compute the number from a list of digits (using first and last digits). When there is one digit, it is multiplied by (10 x + x)
   def compute_number([x]), do: x * 11
   def compute_number([x | r]), do: x * 10 + List.last(r)
 
+  # process one line: scan each character of the string, and build a list of digits
   def process_string([], acc), do: reverse(acc)
 
-  def process_string(s, acc) do
+  def process_string([c | rest] = s, acc) do
+    # check if one of the numbers is a prefix of the string
     case filter(@numbers, fn {k, _} -> List.starts_with?(s, k) end) do
       [] ->
-        if hd(s) in ?0..?9,
-          do: process_string(drop(s, 1), [hd(s) - ?0 | acc]),
-          else: process_string(drop(s, 1), acc)
+        # if not, check if the character is a digit. If so, add it to the list, if not, ignore it. And go to the next character
+        if c in ?0..?9,
+          do: process_string(rest, [c - ?0 | acc]),
+          else: process_string(rest, acc)
 
-      [{number_s, n} | _] when is_list(n) ->
-        process_string(drop(s, length(number_s)), reverse(n) ++ acc)
-
-      [{number_s, n} | _] ->
-        process_string(drop(s, length(number_s)), [n | acc])
+      # if yes, add the corresponding value to the list, and go to the next character
+      [{_, n} | _] ->
+        process_string(rest, [n | acc])
     end
   end
 
+  # process one line: trim the string, convert it to a charlist, and call the recursive function, then compute the number
   def process_string(s) do
     s
     |> String.trim()
@@ -59,12 +67,11 @@ defmodule AdventOfCode.Day01 do
     |> compute_number()
   end
 
-  def part2(args),
-    do:
-      args
-      |> String.split("\n", trim: true)
-      |> map(&process_string/1)
-      |> sum()
-
-  def numbers(), do: @numbers
+  # process each line, and sum the values
+  def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> map(&process_string/1)
+    |> sum()
+  end
 end
