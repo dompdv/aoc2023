49ba388d362468f2898cd0307e6a3649f4eb9e2d
dompdv <dompdv@yahoo.fr>
2023-12-08 22:41:16 +0100

    Day08 P2 clean

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,130 +1,66 @@
 defmodule AdventOfCode.Day08 do
   @lr %{?R => :right, ?L => :left}
 
-  def parse_map(a_map) do
-    a_map
+  # Parse the graph into a map of nodes and their left/right neighbors
+  def parse_graph(raw_graph) do
+    raw_graph
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
-      [a, b, c] = Regex.scan(~r/\w+/, line) |> List.flatten()
-      {a, {b, c}}
+      Regex.scan(~r/\w+/, line) |> List.flatten() |> then(fn [a, b, c] -> {a, {b, c}} end)
     end)
     |> Map.new()
   end
 
+  # Split first line into list of directions and the graph
+  # directions are converted to atoms :right and :left
+  # graph is parsed into a map of nodes and their left/right neighbors
   def parse(args) do
-    [lr, a_map] = args |> String.split("\n\n", trim: true)
+    [lr, raw_graph] = args |> String.split("\n\n", trim: true)
     lr = lr |> String.trim() |> to_charlist |> Enum.map(&Map.get(@lr, &1))
-    {lr, parse_map(a_map)}
+    {lr, parse_graph(raw_graph)}
   end
 
+  # Hop to the left or right neighbor of a node
   def hop(pos, :left, graph), do: graph[pos] |> elem(0)
   def hop(pos, :right, graph), do: graph[pos] |> elem(1)
 
-  def part1(args) do
-    {inst, graph} = args |> parse()
-
-    Stream.cycle(inst)
-    |> Enum.reduce_while({0, "AAA"}, fn dir, {counter, pos} ->
-      next_pos = hop(pos, dir, graph)
-      if next_pos == "ZZZ", do: {:halt, counter + 1}, else: {:cont, {counter + 1, next_pos}}
-    end)
-  end
-
-  def part2(args) do
-    {inst, graph} = args |> parse()
-
-    start_keys =
-      Map.keys(graph) |> Enum.filter(fn key -> String.ends_with?(key, "A") end)
-
-    end_keys =
-      Map.keys(graph) |> Enum.filter(fn key -> String.ends_with?(key, "Z") end) |> MapSet.new()
-
-    {start_keys, end_keys} |> IO.inspect(label: "start/end keys")
-
-    s = Enum.at(start_keys, 2)
-
+  # Same logic as part 1. The stop criteria is different: stop when we reach one of the end nodes
+  def find_cycle_length({inst, graph}, start_node, end_keys) do
     inst
-    |> Enum.with_index()
     |> Stream.cycle()
     |> Enum.reduce_while(
-      {0, s, MapSet.new(), []},
-      fn {dir, c}, {counter, pos, final_states, z_states} ->
+      {0, start_node},
+      fn dir, {counter, pos} ->
         next_pos = hop(pos, dir, graph)
 
-        if MapSet.member?(final_states, {next_pos, c}) do
-          {:halt, {counter + 1, next_pos, c, final_states, z_states}}
-        else
-          z_states =
-            if MapSet.member?(end_keys, next_pos),
-              do: [counter + 1 | z_states],
-              else: z_states
-
-          final_states =
-            if MapSet.member?(end_keys, next_pos),
-              do: MapSet.put(final_states, {next_pos, c}),
-              else: final_states
-
-          {:cont, {counter + 1, next_pos, final_states, z_states}}
-        end
+        if next_pos in end_keys,
+          do: {:halt, counter + 1},
+          else: {:cont, {counter + 1, next_pos}}
       end
     )
   end
 
-  def part21(args) do
-    {inst, graph} = args |> test3() |> parse()
+  # Part 1
+  def part1(args), do: args |> parse() |> find_cycle_length("AAA", ["ZZZ"])
 
-    start_keys =
-      Map.keys(graph) |> Enum.filter(fn key -> String.ends_with?(key, "A") end)
+  # Part 2 specific functions
 
-    end_keys =
-      Map.keys(graph) |> Enum.filter(fn key -> String.ends_with?(key, "Z") end) |> MapSet.new()
+  # smallest common multiple of a list of numbers
+  def ppm([a, b]), do: div(a * b, Integer.gcd(a, b))
+  def ppm([a, b | r]), do: ppm([div(a * b, Integer.gcd(a, b)) | r])
 
-    Stream.cycle(inst)
-    |> Enum.reduce_while({0, start_keys}, fn dir, {counter, pos} ->
-      next_pos = Enum.map(pos, fn p -> hop(p, dir, graph) end)
+  # Part 2
+  # The smallest global cycle length is the smallest common multiple of the cycle lengths of each node
+  def part2(args) do
+    {inst, graph} = args |> parse()
 
-      if Enum.all?(next_pos, fn p -> MapSet.member?(end_keys, p) end),
-        do: {:halt, counter + 1},
-        else: {:cont, {counter + 1, next_pos}}
-    end)
+    # Find all start and end nodes
+    nodes = Map.keys(graph)
+    start_keys = Enum.filter(nodes, &String.ends_with?(&1, "A"))
+    end_keys = Enum.filter(nodes, &String.ends_with?(&1, "Z"))
 
-    {start_keys, end_keys}
+    start_keys
+    |> Enum.map(&find_cycle_length({inst, graph}, &1, end_keys))
+    |> ppm()
   end
-
-  def test(_) do
-    """
-    RL
-
-    AAA = (BBB, CCC)
-    BBB = (DDD, EEE)
-    CCC = (ZZZ, GGG)
-    DDD = (DDD, DDD)
-    EEE = (EEE, EEE)
-    GGG = (GGG, GGG)
-    ZZZ = (ZZZ, ZZZ)
-    """
-  end
-
-  def test2(_),
-    do: """
-    LLR
-
-    AAA = (BBB, BBB)
-    BBB = (AAA, ZZZ)
-    ZZZ = (ZZZ, ZZZ)
-    """
-
-  def test3(_),
-    do: """
-    LR
-
-    11A = (11B, XXX)
-    11B = (XXX, 11Z)
-    11Z = (11B, XXX)
-    22A = (22B, XXX)
-    22B = (22C, 22C)
-    22C = (22Z, 22Z)
-    22Z = (22B, 22B)
-    XXX = (XXX, XXX)
-    """
 end
