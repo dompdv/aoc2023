235bb1718f0125f882b254b30e84764704457567
dompdv <dompdv@yahoo.fr>
2023-12-09 07:48:02 +0100

    Day 09 shortened

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,22 +1,24 @@
 defmodule AdventOfCode.Day09 do
-  def parse_line(line),
-    do: line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+  import Enum
 
-  def parse(input), do: input |> String.split("\n", trim: true) |> Enum.map(&parse_line/1)
+  def parse_line(line),
+    do: line |> String.split(" ", trim: true) |> map(&String.to_integer/1)
+
+  def parse(input), do: input |> String.split("\n", trim: true) |> map(&parse_line/1)
 
   def apply_diffs(l), do: apply_diffs(l, [l])
 
   def apply_diffs(l, acc) do
-    d = l |> Enum.chunk_every(2, 1, :discard) |> Enum.map(fn [a, b] -> b - a end)
-    if Enum.all?(d, &(&1 == 0)), do: acc, else: apply_diffs(d, [d | acc])
+    d = l |> chunk_every(2, 1, :discard) |> map(fn [a, b] -> b - a end)
+    if all?(d, &(&1 == 0)), do: acc, else: apply_diffs(d, [d | acc])
   end
 
-  def predict1(steps), do: steps |> Enum.map(&List.last/1) |> Enum.sum()
-  def predict2(steps), do: Enum.reduce(steps, 0, fn l, previous -> hd(l) - previous end)
+  def predict1(steps), do: steps |> map(&List.last/1) |> sum()
+  def predict2(steps), do: reduce(steps, 0, fn l, previous -> hd(l) - previous end)
 
   def part1(args),
-    do: args |> parse() |> Enum.map(&apply_diffs/1) |> Enum.map(&predict1/1) |> Enum.sum()
+    do: args |> parse() |> map(&apply_diffs/1) |> map(&predict1/1) |> sum()
 
   def part2(args),
-    do: args |> parse() |> Enum.map(&apply_diffs/1) |> Enum.map(&predict2/1) |> Enum.sum()
+    do: args |> parse() |> map(&apply_diffs/1) |> map(&predict2/1) |> sum()
 end
