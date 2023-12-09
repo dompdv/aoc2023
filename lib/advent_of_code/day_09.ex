20fa4f2af7a45c1ed8572072ab9cea2ea867d2ec
dompdv <dompdv@yahoo.fr>
2023-12-09 07:37:58 +0100

    Day09 P2

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -14,14 +14,14 @@ defmodule AdventOfCode.Day09 do
     if Enum.all?(d, &(&1 == 0)), do: acc, else: apply_diffs(d, [d | acc])
   end
 
-  def predict(steps), do: steps |> Enum.map(&List.last/1) |> Enum.sum()
+  def predict1(steps), do: steps |> Enum.map(&List.last/1) |> Enum.sum()
+  def predict2(steps), do: Enum.reduce(steps, 0, fn l, previous -> hd(l) - previous end)
 
-  def part1(args) do
-    args |> parse() |> Enum.map(&apply_diffs/1) |> Enum.map(&predict/1) |> Enum.sum()
-  end
+  def part1(args),
+    do: args |> parse() |> Enum.map(&apply_diffs/1) |> Enum.map(&predict1/1) |> Enum.sum()
 
-  def part2(_args) do
-  end
+  def part2(args),
+    do: args |> parse() |> Enum.map(&apply_diffs/1) |> Enum.map(&predict2/1) |> Enum.sum()
 
   def test(_),
     do: """
