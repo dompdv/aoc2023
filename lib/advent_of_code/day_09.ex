398fbe5314476c2d5a5268b4a6f194bcc9f11200
dompdv <dompdv@yahoo.fr>
2023-12-09 07:47:00 +0100

    Day 09 shortened

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -4,13 +4,10 @@ defmodule AdventOfCode.Day09 do
 
   def parse(input), do: input |> String.split("\n", trim: true) |> Enum.map(&parse_line/1)
 
-  def make_diff(l), do: l |> Enum.chunk_every(2, 1, :discard) |> Enum.map(fn [a, b] -> b - a end)
-
   def apply_diffs(l), do: apply_diffs(l, [l])
-  def apply_diffs([_], acc), do: {:nok, acc}
 
   def apply_diffs(l, acc) do
-    d = make_diff(l)
+    d = l |> Enum.chunk_every(2, 1, :discard) |> Enum.map(fn [a, b] -> b - a end)
     if Enum.all?(d, &(&1 == 0)), do: acc, else: apply_diffs(d, [d | acc])
   end
 
@@ -22,11 +19,4 @@ defmodule AdventOfCode.Day09 do
 
   def part2(args),
     do: args |> parse() |> Enum.map(&apply_diffs/1) |> Enum.map(&predict2/1) |> Enum.sum()
-
-  def test(_),
-    do: """
-    0 3 6 9 12 15
-    1 3 6 10 15 21
-    10 13 16 21 30 45
-    """
 end
