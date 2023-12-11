de693aef4608462854f1ded958e1b25c8903210e
dompdv <dompdv@yahoo.fr>
2023-12-11 18:58:49 +0100

    Day10 p2

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -5,36 +5,21 @@ defmodule AdventOfCode.Day10 do
   @dir [:west, :east, :north, :south]
   @delta_dir zip(@dir, @neighbors)
 
-  def print(graph, cycle) do
-    columns = for({{_, c}, _} <- graph, do: c) |> max()
-    rows = for({{r, _}, _} <- graph, do: r) |> max()
-
-    for r <- 0..rows do
-      for c <- 0..columns do
-        cell = {r, c}
-
-        if cell in cycle,
-          do: IO.write("X"),
-          else: IO.write(if graph[cell] == [], do: ".", else: "#")
-      end
-
-      IO.puts("")
-    end
-  end
+  @cell_types %{
+    ?. => [],
+    ?- => [:east, :west],
+    ?| => [:south, :north],
+    ?L => [:north, :east],
+    ?J => [:north, :west],
+    ?7 => [:south, :west],
+    ?F => [:south, :east],
+    ?S => :start
+  }
 
   def parse_line(line) do
     line
     |> to_charlist()
-    |> Enum.map(fn
-      ?. -> []
-      ?- -> [:east, :west]
-      ?| -> [:south, :north]
-      ?L -> [:north, :east]
-      ?J -> [:north, :west]
-      ?7 -> [:south, :west]
-      ?F -> [:south, :east]
-      ?S -> :start
-    end)
+    |> Enum.map(&@cell_types[&1])
     |> with_index()
   end
 
@@ -78,6 +63,19 @@ defmodule AdventOfCode.Day10 do
     |> filter(&(&1 != nil))
   end
 
+  def reveal_start(graph, start) do
+    @dir
+    |> map(fn dir ->
+      adjacent_cell = delta(start, @delta_dir[dir])
+      connections = Map.get(graph, adjacent_cell, [])
+
+      if any?(connections, &check_connected(dir, &1)),
+        do: dir,
+        else: nil
+    end)
+    |> filter(&(&1 != nil))
+  end
+
   def search_cycle([], _, visited), do: visited
 
   def search_cycle([node | to_visit], graph, visited) do
@@ -93,66 +91,65 @@ defmodule AdventOfCode.Day10 do
     search_cycle([start], graph, []) |> length() |> div(2)
   end
 
-  def segments(l), do: chunk_every(l ++ [hd(l)], 2, 1)
+  # Point A : start of the semi-line, V1 : direction of the semi-line, Point 1 and 2 : segment
+  def intersect({xa, ya}, {xv1, yv1}, {x1, y1}, {x2, y2}) do
+    {xv2, yv2} = {x2 - x1, y2 - y1}
+    num = (xa - x1) * yv2 - (ya - y1) * xv2
+    den = xv2 * yv1 - yv2 * xv1
+    t = num / den
+
+    # Check if the intersection point is on the semi-line
+    if t >= 0 do
+      # Check if the intersection point is on the segment
+      {xi, yi} = {xa + xv1 * t, ya + yv1 * t}
+      (x1 - xi) * (x2 - xi) + (y1 - yi) * (y2 - yi) <= 0
+    else
+      false
+    end
+  end
+
+  # Draw precise segments (with 0.5 offset)
+  def to_segment(graph, {r, c} = cell) do
+    map(
+      graph[cell],
+      fn
+        :east -> {{r + 0.5, c + 0.5}, {r + 0.5, c + 1}}
+        :west -> {{r + 0.5, c + 0.5}, {r + 0.5, c}}
+        :north -> {{r + 0.5, c + 0.5}, {r, c + 0.5}}
+        :south -> {{r + 0.5, c + 0.5}, {r + 1, c + 0.5}}
+      end
+    )
+  end
+
+  # Accumulate segments for all the cells in a list (here in the cycle)
+  def to_segments(graph, l), do: map(l, &to_segment(graph, &1)) |> List.flatten()
+
+  def count_intersections(start, direction, segments) do
+    segments
+    |> map(fn {p1, p2} -> intersect(start, direction, p1, p2) end)
+    |> filter(& &1)
+    |> length()
+  end
+
+  # Offset the start point and choose a direction which is not parallel to the axes and with very low probably to encounter a (0.5) point (wich would be a corner, thus on 2 segments)
+  def count_intersections({r, c}, segments),
+    do: count_intersections({r + 0.509, c + 0.49}, {2.28787, 1.33424545}, segments)
+
+  def odd?(n), do: rem(n, 2) == 1
 
   def part2(args) do
-    {graph, start} = args |> test() |> parse()
-    cycle = search_cycle([start], graph, []) |> IO.inspect()
-    print(graph, cycle)
-    cycle |> IO.inspect() |> segments() |> IO.inspect()
-  end
+    {graph, start} = args |> parse()
+    # Change the start cell to its real shape
+    graph = Map.put(graph, start, reveal_start(graph, start))
+    # Identify the cycle
+    cycle = search_cycle([start], graph, [])
+    # compute the segments
+    segments = to_segments(graph, cycle)
 
-  def test(_) do
-    """
-    .....
-    .S-7.
-    .|.|.
-    .L-J.
-    .....
-    """
-  end
-
-  def test2(_) do
-    """
-    ...........
-    .S-------7.
-    .|F-----7|.
-    .||.....||.
-    .||.....||.
-    .|L-7.F-J|.
-    .|..|.|..|.
-    .L--J.L--J.
-    ...........
-    """
-  end
-
-  def test3(_) do
-    """
-    FF7FSF7F7F7F7F7F---7
-    L|LJ||||||||||||F--J
-    FL-7LJLJ||||||LJL-77
-    F--JF--7||LJLJIF7FJ-
-    L---JF-JLJIIIIFJLJJ7
-    |F|F-JF---7IIIL7L|7|
-    |FFJF7L7F-JF7IIL---7
-    7-L-JL7||F7|L7F-7F7|
-    L.L7LFJ|||||FJL7||LJ
-    L7JLJL-JLJLJL--JLJ.L
-    """
-  end
-
-  def test4(_) do
-    """
-    .F----7F7F7F7F-7....
-    .|F--7||||||||FJ....
-    .||.FJ||||||||L7....
-    FJL7L7LJLJ||LJ.L-7..
-    L--J.L7...LJS7F-7L7.
-    ....F-J..F7FJ|L7L7L7
-    ....L7.F7||L7|.L7L7|
-    .....|FJLJ|FJ|F7|.LJ
-    ....FJL-7.||.||||...
-    ....L---J.LJ.LJLJ...
-    """
+    # For each node in the cycle, count the number of intersections with the segments. If odd, it an inside point
+    graph
+    |> filter(fn {c, _} -> c not in cycle end)
+    |> filter(fn {cell, _v} -> odd?(count_intersections(cell, segments)) end)
+    |> length()
   end
 end
