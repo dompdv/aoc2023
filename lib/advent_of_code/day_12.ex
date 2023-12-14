f1f813c0de3b81264d6a0cf19f12177fda8260fb
dompdv <dompdv@yahoo.fr>
2023-12-14 23:45:47 +0100

    Day12 P1 & P2 clean

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -9,10 +9,34 @@ defmodule AdventOfCode.Day12 do
     {row |> to_charlist(), numbers}
   end
 
+  # Memoization store
+
+  def create_memoization_store() do
+    # Delete the memoization store if it exists
+    if :ets.whereis(:memo) != :undefined, do: :ets.delete(:memo)
+    :ets.new(:memo, [:set, :protected, :named_table])
+  end
+
+  def store(value, key) do
+    :ets.insert(:memo, {key, value})
+    value
+  end
+
+  def retrieve(key) do
+    case :ets.lookup(:memo, key) do
+      [{_, result}] -> result
+      [] -> nil
+    end
+  end
+
   # End of the characters
+  # There is no more numbers and the counter is 0: we have a solution
   def solve([], 0, []), do: 1
+  # There is no more numbers but the counter is not 0: dead end
   def solve([], _counter, []), do: 0
+  # The remaining number is the current counter: we have a solution
   def solve([], counter, [counter]), do: 1
+  # The remaining number is not the current counter: dead end
   def solve([], _counter, _numbers), do: 0
 
   # Processing a . character following another . or the start of the line: just move on
@@ -33,39 +57,17 @@ defmodule AdventOfCode.Day12 do
     if counter >= h, do: 0, else: solve(line, counter + 1, numbers)
   end
 
-  # Processing a ? character adding the two cases
+  # Processing a ? character by adding the two cases
   def solve([?? | line] = l, counter, numbers) do
+    # Try to retrieve from the memoization store
     case retrieve({l, counter, numbers}) do
       nil ->
-        result =
-          if length(line) + counter + 1 < sum(numbers) + length(numbers) - 1,
-            do: 0,
-            else: solve([?# | line], counter, numbers) + solve([?. | line], counter, numbers)
-
-        store({l, counter, numbers}, result)
+        # If not found, compute the result (sum of the 2 cases: . or #) and store it
+        (solve([?# | line], counter, numbers) + solve([?. | line], counter, numbers))
+        |> store({l, counter, numbers})
 
       result ->
         result
-    end
-  end
-
-  def create_memoization_store() do
-    if :ets.whereis(:memo) != :undefined, do: :ets.delete(:memo)
-    :ets.new(:memo, [:set, :protected, :named_table])
-  end
-
-  def store(key, value) do
-    :ets.insert(:memo, {key, value})
-    value
-  end
-
-  def retrieve(key) do
-    case :ets.lookup(:memo, key) do
-      [{_, result}] ->
-        result
-
-      [] ->
-        nil
     end
   end
 
@@ -82,15 +84,4 @@ defmodule AdventOfCode.Day12 do
   def part1(args), do: args |> parse() |> map(&solve/1) |> sum()
 
   def part2(args), do: args |> parse() |> map(&unfold_paper/1) |> map(&solve/1) |> sum()
-
-  def test(_) do
-    """
-    ???.### 1,1,3
-    .??..??...?##. 1,1,3
-    ?#?#?#?#?#?#?#? 1,3,1,6
-    ????.#...#... 4,1,1
-    ????.######..#####. 1,6,5
-    ?###???????? 3,2,1
-    """
-  end
 end
