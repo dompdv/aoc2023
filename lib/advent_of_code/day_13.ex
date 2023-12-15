4eb3d3d275972c1a87bc535707259562893825e2
dompdv <dompdv@yahoo.fr>
2023-12-15 07:55:00 +0100

    Day13 p1

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -27,29 +27,40 @@ defmodule AdventOfCode.Day13 do
     {lines, rows}
   end
 
-  def sym_list(list) do
-    # |> map(fn [a, b] -> a == b end)
+  def pivot?({i, j}, list) do
+    {l, r} = split(list, i + 1) |> IO.inspect()
+    cut_to = Kernel.min(length(r), length(l))
+    l = Enum.reverse(l)
+    IO.inspect({Enum.slice(l, 0, cut_to), Enum.slice(r, 0, cut_to)})
+    Enum.slice(l, 0, cut_to) == Enum.slice(r, 0, cut_to)
+  end
+
+  def identitfy_pivots(list) do
     list
     |> with_index()
     |> chunk_every(2, 1, :discard)
     |> filter(fn [{x, _}, {y, _}] -> x == y end)
     |> map(fn [{_, i}, {_, j}] -> {i, j} end)
+    |> filter(&pivot?(&1, list))
+  end
+
+  def sym_list(list) do
+    identitfy_pivots(list)
   end
 
   def detect_symmetry({lines, rows}) do
-    sym_list(lines)
-    sym_list(rows)
+    case {sym_list(lines), sym_list(rows)} do
+      {[], []} -> :none
+      {[{_, r}], []} -> 100 * r
+      {[], [{_, l}]} -> l
+      _ -> :both
+    end
   end
 
   def parse(args), do: args |> String.split("\n\n", trim: true) |> map(&parse_pattern/1)
 
   def part1(args) do
-    args
-    |> test()
-    |> parse()
-    |> map(&process_pattern/1)
-    |> IO.inspect()
-    |> map(&detect_symmetry/1)
+    args |> parse() |> map(&process_pattern/1) |> map(&detect_symmetry/1) |> sum()
   end
 
   def part2(_args) do
