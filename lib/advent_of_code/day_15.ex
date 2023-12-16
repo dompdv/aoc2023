a79dbba79ea3c591b00d59853bbc74f651bf99a2
dompdv <dompdv@yahoo.fr>
2023-12-16 21:15:48 +0100

    Day15 P1

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,5 +1,19 @@
 defmodule AdventOfCode.Day15 do
-  def part1(_args) do
+  import Enum
+
+  def hash(item), do: hash(to_charlist(item), 0)
+  def hash([], v), do: v
+  def hash([13 | t], v), do: hash(t, v)
+  def hash([h | t], v), do: hash(t, rem((v + h) * 17, 256))
+
+  def part1(args) do
+    "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
+    args
+    |> String.replace("\n", "")
+    |> String.split(",", trim: true)
+    |> map(&hash/1)
+    |> sum()
   end
 
   def part2(_args) do
