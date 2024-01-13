b01f3e54649b29aeea67730fe046acc15e39033c
dompdv <dompdv@yahoo.fr>
2024-01-14 00:32:15 +0100

    Day22 p1 & p2 clean

--- lib/advent_of_code/day_22.ex
+++ lib/advent_of_code/day_22.ex
@@ -1,96 +1,83 @@
 defmodule AdventOfCode.Day22 do
   import Enum
 
-  def to_structure([[x, y, z], [x, y, z]]), do: {z, 1, :z, [[x, y]]}
-  def to_structure([[l, y, z], [h, y, z]]), do: {z, 1, :x, for(i <- l..h, do: [i, y])}
-  def to_structure([[x, l, z], [x, h, z]]), do: {z, 1, :y, for(i <- l..h, do: [x, i])}
-  def to_structure([[x, y, l], [x, y, h]]), do: {l, h - l + 1, :z, [[x, y]]}
+  ### Parsing
 
+  # Parse a list of integers separated by commas
   def intlist(l), do: l |> String.split(",", trim: true) |> map(&String.to_integer/1)
+  # Parse a line
   def parse_line(line), do: String.split(line, "~") |> map(&intlist/1)
 
+  # Parse the input
   def parse(args),
     do: args |> String.split("\n", trim: true) |> map(&parse_line/1)
 
+  # Represents a brick with a tuple {z of the lowest point, height, list of [x, y] coordinates of the brick}
+  def to_structure([[x, y, z], [x, y, z]]), do: {z, 1, [[x, y]]}
+  def to_structure([[l, y, z], [h, y, z]]), do: {z, 1, for(i <- l..h, do: [i, y])}
+  def to_structure([[x, l, z], [x, h, z]]), do: {z, 1, for(i <- l..h, do: [x, i])}
+  def to_structure([[x, y, l], [x, y, h]]), do: {l, h - l + 1, [[x, y]]}
+
+  # Convert a list of bricks to a list of structures
   def prepare(cubes), do: cubes |> map(&to_structure/1)
 
-  def field_size(cubes) do
-    corners = cubes |> map(fn {_, l, _} -> zip(l) end)
+  ## Part 1 & 2
 
-    for dim <- 0..2 do
-      corners |> map(fn c -> at(c, dim) |> Tuple.to_list() end) |> List.flatten() |> min_max()
-    end
-  end
-
+  # Sort cubes by their lowest point
   def sort_cubes(cubes), do: cubes |> Enum.sort_by(&elem(&1, 0))
 
-  def height(elevation, x, y), do: Map.get(elevation, {x, y}, 0)
-
+  # Main function to fall the cubes
   def fall(sorted_cubes) do
-    {_, fell, has_moved} =
-      Enum.reduce(sorted_cubes, {%{}, [], 0}, fn cube, {elevations, cubes, moved} ->
-        {l, h, t, xys} = cube
-        highest = max(for([x, y] <- xys, do: height(elevations, x, y)))
+    # Cubes have to be sorted by increasing lowest point
+    sorted_cubes
+    # Loop over the cubes
+    # elevations: map of [x, y] coordinates to their elevation (keep a running map of the highest elevation of each x,y coordinate)
+    # cubes: list of cubes that has been processed
+    # moved: number of cubes that have been moved so far
+    |> Enum.reduce({%{}, [], 0}, fn cube, {elevations, cubes, moved} ->
+      {l, h, xys} = cube
+      # Find the highest elevation found so far for the x,y of the cube
+      highest = max(for([x, y] <- xys, do: Map.get(elevations, {x, y}, 0)))
+      # Compute the new elevations for the x,y of the cube
+      new_elevations =
+        Enum.reduce(xys, elevations, fn [x, y], acc ->
+          Map.put(acc, {x, y}, highest + h)
+        end)
 
-        new_elevations =
-          Enum.reduce(xys, elevations, fn [x, y], acc ->
-            Map.put(acc, {x, y}, highest + h)
-          end)
-
-        if l == highest + 1,
-          do: {new_elevations, [cube | cubes], moved},
-          else: {new_elevations, [{highest + 1, h, t, xys} | cubes], moved + 1}
-      end)
-
-    {reverse(fell), has_moved}
+      # If the cube is already at the highest elevation, add it to the list of processed cubes
+      # else modify the elevation of the cube to the highest elevation + 1 and add it to the list of cubes to process
+      # and increment the number of moved cubes
+      if l == highest + 1,
+        do: {new_elevations, [cube | cubes], moved},
+        else: {new_elevations, [{highest + 1, h, xys} | cubes], moved + 1}
+    end)
+    # Return the list of processed cubes in a sorted order and the number of moved cubes
+    |> then(fn {_, l, n} -> {reverse(l), n} end)
   end
 
-  def something_will_move(sorted_cubes) do
-    will_move =
-      Enum.reduce_while(sorted_cubes, %{}, fn cube, elevations ->
-        {l, h, _t, xys} = cube
-        highest = max(for([x, y] <- xys, do: height(elevations, x, y)))
-
-        if l == highest + 1 do
-          {:cont,
-           Enum.reduce(xys, elevations, fn [x, y], acc ->
-             Map.put(acc, {x, y}, highest + h)
-           end)}
-        else
-          {:halt, :move}
-        end
-      end)
-
-    will_move == :move
-  end
-
-  def can_desintegrate(i, fell),
-    do: fell |> List.delete_at(i) |> something_will_move() |> then(&(not &1))
-
+  # Part 1
   def part1(args) do
+    # Parse the input, convert it to a list of structures, sort it by increasing lowest point and fall the cubes to their starting positions
     fell = args |> parse() |> prepare() |> sort_cubes() |> fall() |> elem(0)
 
-    for(i <- 0..(length(fell) - 1), can_desintegrate(i, fell), do: 1) |> sum()
+    # For each cube, remove it from the list of cubes and fall the remaining cubes and count the number of cubes that have fallen in each case
+    for i <- 0..(length(fell) - 1) do
+      fell |> List.delete_at(i) |> fall() |> elem(1)
+    end
+    # Count the cubes for which the number of cubes that have fallen is 0
+    |> filter(&(&1 == 0))
+    |> count()
   end
 
+  # Part 2
   def part2(args) do
+    # See part 1
     fell = args |> parse() |> prepare() |> sort_cubes() |> fall() |> elem(0)
 
     for i <- 0..(length(fell) - 1) do
       fell |> List.delete_at(i) |> fall() |> elem(1)
     end
+    # Sum the number of cubes that have fallen in each case
     |> sum()
-  end
-
-  def test(_) do
-    """
-    1,0,1~1,2,1
-    0,0,2~2,0,2
-    0,2,3~2,2,3
-    0,0,4~0,2,4
-    2,0,5~2,2,5
-    0,1,6~2,1,6
-    1,1,8~1,1,9
-    """
   end
 end
