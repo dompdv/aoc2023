2f682261b6cd74a82e032942cf57d896b8c5d322
dompdv <dompdv@yahoo.fr>
2024-01-15 09:42:35 +0100

    Day20 p1

--- lib/advent_of_code/day_20.ex
+++ lib/advent_of_code/day_20.ex
@@ -56,36 +56,37 @@ defmodule AdventOfCode.Day20 do
     |> Map.new()
   end
 
-  def inc_low(counter), do: %{counter | low: 1 + counter[:low]}
-  def inc_high(counter), do: %{counter | high: 1 + counter[:high]}
-  def inc_signal(counter, signal), do: %{counter | signal => 1 + counter[signal]}
+  def inc_low(counter, n \\ 1), do: %{counter | low: n + counter[:low]}
+  def inc_high(counter, n \\ 1), do: %{counter | high: n + counter[:high]}
+  def inc_signal(counter, signal, n \\ 1), do: %{counter | signal => n + counter[signal]}
 
   # Flip the state of a flipflop
   def flip(:on), do: :off
   def flip(:off), do: :on
-  def flip(state, node), do: Map.put(state, node, flip(Map.get(state, node)))
+  def flip(state, node), do: Map.put(state, node, flip(state[node]))
 
   # Send a signal to all successors
-  def broadcast(signal, successors, from), do: for(to <- successors, do: {to, signal, from})
+  def broadcast(signal, from, successors), do: for(to <- successors, do: {from, to, signal})
 
   def all_high?(l), do: all?(Map.values(l), &(&1 == :high))
   # Execute one hop of the signal
   # ie process one pulse
   # A pulse is a tuple {to_node, signal, from_node}
   # Returns a tuple {new_pulses, new_state, new_counter}
-  def hop({node, signal, from_node}, state, counter, graph) do
-    IO.inspect({node, signal, from_node, state, counter}, label: "Hop")
-    node_state = state[node]
-    {node_type, successors} = Map.get(graph, node, {nil, nil})
+  def hop({from_node, to_node, signal}, state, counter, graph) do
+    #    IO.inspect({from_node, to_node, signal, state, counter}, label: "Hop")
+    node_state = state[to_node]
+    {node_type, successors} = Map.get(graph, to_node, {nil, nil})
+    ns = if successors == nil, do: 0, else: length(successors)
 
     cond do
       # button, send a low signal to :broadcaster and increment the low counter
       node_type == :button ->
-        {[{:broadcaster, :low, :button}], state, inc_low(counter)}
+        {[{:button, :broadcaster, :low}], state, inc_low(counter)}
 
       # broadcaster, transmit the signal to all successors and increment the signal counter
       node_type == :broadcaster ->
-        {broadcast(signal, successors, node), state, inc_signal(counter, signal)}
+        {broadcast(signal, to_node, successors), state, inc_signal(counter, signal, ns)}
 
       # end node, stop the signal
       node_state == nil ->
@@ -99,23 +100,21 @@ defmodule AdventOfCode.Day20 do
       node_type == :flipflop ->
         signal_to_send = if node_state == :off, do: :high, else: :low
 
-        {broadcast(signal_to_send, successors, node), flip(state, node),
-         inc_signal(counter, signal_to_send)}
+        {broadcast(signal_to_send, to_node, successors), flip(state, to_node),
+         inc_signal(counter, signal_to_send, ns)}
 
       # conj
       node_type == :conj ->
-        # update memory of the node
+        # update node memory
         new_node_state =
-          Map.put(node_state, from_node, signal) |> IO.inspect(label: "New node state")
+          Map.put(node_state, from_node, signal)
 
-        new_state = Map.put(state, node, new_node_state)
+        new_state = Map.put(state, to_node, new_node_state)
 
         # if all inputs are high, send a low signal to all successors, else send a high signal
-        if all_high?(new_node_state) do
-          {broadcast(:low, successors, node), new_state, inc_low(counter)}
-        else
-          {broadcast(:high, successors, node), new_state, inc_high(counter)}
-        end
+        if all_high?(new_node_state),
+          do: {broadcast(:low, to_node, successors), new_state, inc_low(counter, ns)},
+          else: {broadcast(:high, to_node, successors), new_state, inc_high(counter, ns)}
     end
   end
 
@@ -123,14 +122,13 @@ defmodule AdventOfCode.Day20 do
   def tick(pulses, state, counter, graph) do
     reduce(pulses, {[], state, counter}, fn pulse, {acc_pulses, st, ct} ->
       {new_pulses, new_state, new_counter} = hop(pulse, st, ct, graph)
-      IO.inspect(new_pulses, label: "New pulses")
       {[acc_pulses | new_pulses], new_state, new_counter}
     end)
     # acc_pulses are list, so flatten them
     |> then(fn {pulses, s, c} -> {List.flatten(pulses), s, c} end)
   end
 
-  def initial_pulse(), do: [{:button, :low, :elve}]
+  def initial_pulse(), do: [{:elve, :button, :low}]
   def run([], state, counter, _graph), do: {state, counter}
 
   def run(pulses, state, counter, graph) do
@@ -140,8 +138,12 @@ defmodule AdventOfCode.Day20 do
   end
 
   def part1(args) do
-    graph = args |> test() |> parse()
-    run(initial_pulse(), inital_state(graph), %{low: 0, high: 0}, graph)
+    graph = args |> parse()
+
+    reduce(1..1000, {inital_state(graph), %{low: 0, high: 0}}, fn _i, {state, counter} ->
+      run(initial_pulse(), state, counter, graph)
+    end)
+    |> then(fn {_state, counter} -> counter[:low] * counter[:high] end)
   end
 
   def part2(_args) do
